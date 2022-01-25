91e4b72347965baaf29c98f25a62bff641a23b75
iloveconordubh <57184309+conor4sc@users.noreply.github.com>
2022-01-25 16:04:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function validate(){
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if(username=="user1"&& password=="password")
+  {
+    alert("login successful");
+
+
+  
+    }else{
+      alert("login unsuccessful");
+    }
+
+}
